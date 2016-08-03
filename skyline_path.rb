34ab2adce0065b6a3c137cb9cc869117814781df
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-08-03 18:22:53 +0800

    update skyline path core from other project

--- skyline_path.rb
+++ skyline_path.rb
@@ -16,24 +16,62 @@ class SkylinePath < Graph
     @part_skyline_path = {}
   end
 
-  def query_skyline_path(src_id: nil, dst_id: nil, limit: nil)
+  def query_skyline_path(src_id: nil, dst_id: nil, limit: nil, evaluate: false)
     @distance_limit = limit
     query_check(src_id, dst_id)
-
-    Benchmark.benchmark(CAPTION, 22, FORMAT, 'total:') do |step|
-      t1 = step.report('Shorest path') do
-        shorest_path = shorest_path_query(src_id, dst_id)
-        @skyline_path[path_to_sym(shorest_path)] = attrs_in(shorest_path)
-        @shorest_distance = attrs_in(shorest_path).first
+    if evaluate
+      Benchmark.benchmark(CAPTION, 22, FORMAT, 'total:') do |step|
+        t1 = step.report('Shorest path') do
+          shorest_path = shorest_path_query(src_id, dst_id)
+          @skyline_path[path_to_sym(shorest_path)] = attrs_in(shorest_path)
+          @shorest_distance = attrs_in(shorest_path).first
+        end
+        t2 = step.report('SkyPath') do
+          sky_path(src_id, dst_id)
+        end
+        [t1 + t2]
       end
-      t2 = step.report('SkyPath') do
-        sky_path(src_id, dst_id)
-      end
-      [t1 + t2]
+    else
+      shorest_path = shorest_path_query(src_id, dst_id)
+      raise "Can't find any road between #{src_id} and #{dst_id}"  if shorest_path.nil?
+      @skyline_path[path_to_sym(shorest_path)] = attrs_in(shorest_path)
+      @shorest_distance = attrs_in(shorest_path).first
+      sky_path(src_id, dst_id)
     end
-    puts "Found #{@skyline_path.size} Skyline paths"
+
+    # puts "Found #{@skyline_path.size} Skyline paths"
     @skyline_path
   end
+
+  def get_all_paths(params)
+    src = params[:src_id]
+    dst = params[:dst_id]
+    find_paths(src, dst)
+  end
+
+  def find_paths(src, dst)
+    path_recursive(src, dst, {}, [])
+  end
+
+  def path_recursive(cur, dst, paths, path)
+    path << cur
+    if cur == dst
+      path = get_dst(cur, paths, path)
+      return
+    end
+    find_neighbors(cur).each do |n|
+      path_recursive(n, dst, paths, path) unless path.include?(n)
+    end
+    path.delete(cur)
+    paths
+  end
+
+  def get_dst(cur, paths, path)
+    paths[path_to_sym(path)] = attrs_in(path)
+    path.delete(cur)
+  end
+
+  protected
 
   def attrs_in(path)
     if path.size > 2
@@ -47,7 +85,9 @@ class SkylinePath < Graph
     attr_full
   end
 
-  private
+  def attr_between(src, dst)
+    find_edge(src, dst).attrs
+  end
 
   def sky_path(cur, dst, pass = [], cur_attrs = Array.new(@dim, 0))
     pass << cur
@@ -78,18 +118,19 @@ class SkylinePath < Graph
     end
 
     non_skyline.each { |key| @skyline_path.delete(key) }
-
     @skyline_path[path_to_sym(pass)] = attrs if new_skyline_flag
   end
+
+  private
 
   def next_hop?(n, pass, next_path_attrs)
     unless @distance_limit.nil?
       return false if out_of_limit?(next_path_attrs.first)
     end
     return false if pass.include?(n)
-    partial_result = partial_dominance?(pass + [n], next_path_attrs)
+    partial_result = partial_dominance?(n, next_path_attrs)
     return false if partial_result
-    add_part_skyline(pass + [n], next_path_attrs) unless partial_result.nil?
+    add_part_skyline(n, next_path_attrs)
     return false if full_dominance?(next_path_attrs)
     true
   end
@@ -98,11 +139,9 @@ class SkylinePath < Graph
     distance > @shorest_distance * @distance_limit ? true : false
   end
 
-  def partial_dominance?(path, path_attrs)
-    sym = "p#{path.first}_#{path.last}".to_sym
-    result = false
-    unless @part_skyline_path[sym].nil?
-      result = @part_skyline_path[sym].dominate?(path_attrs)
+  def partial_dominance?(target, path_attrs)
+    unless @part_skyline_path[target].nil?
+      result = @part_skyline_path[target].dominate?(path_attrs)
     end
     result
   end
@@ -114,13 +153,9 @@ class SkylinePath < Graph
     false # not be dominance
   end
 
-  def add_part_skyline(path, path_attrs)
-    sym = "p#{path.first}_#{path.last}".to_sym
-    @part_skyline_path[sym] = path_attrs
-  end
-
-  def attr_between(src, dst)
-    find_edge(src, dst).attrs
+  def add_part_skyline(target, path_attrs)
+    return unless @part_skyline_path[target].nil?
+    @part_skyline_path[target] = path_attrs
   end
 
   def query_check(src_id, dst_id)
@@ -140,6 +175,4 @@ class SkylinePath < Graph
     raise ArgumentError, 'path must be Array' unless path.class == Array
     "p#{path.join('_')}".to_sym
   end
-
-
 end
